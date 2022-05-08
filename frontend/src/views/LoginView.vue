61d45395a6abb636454871a02fa2697c255df75d
alan910127 <alan469357@gmail.com>
2022-05-08 16:55:03 +0800

    LoginView

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="login">
-<<<<<<< HEAD
     <h1>Login Page</h1>
     <signin-page v-if="user === null"></signin-page>
     <signup-page v-else></signup-page>
-=======
-    <h1>This is Login Page</h1>
->>>>>>> d9b4107458d39be32478cad18277ee3490f7b3ce
   </div>
 </template>
 
