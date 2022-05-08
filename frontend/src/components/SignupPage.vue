359550a14cdd8e0cb7c64037bb4785cf7666cfc7
vinchantle <chenvincent610@gmail.com>
2022-05-08 17:59:38 +0800

    updated SignupPage.vue

--- frontend/src/components/SignupPage.vue
+++ frontend/src/components/SignupPage.vue
@@ -2,10 +2,16 @@
   <div class="sign">
     <h3>Sign Up</h3>
     <form @submit.prevent="onSubmit">
+      <input v-model="realname" type="text" placeholder="Real Name" />
       <input v-model="account" type="text" placeholder="Account" />
+      <input v-model="phone" type="text" placeholder="Phone Number" />
       <input v-model="password" type="password" placeholder="Password" />
+      <input v-model="retype" type="password" placeholder="Retype Password" />
+      <input v-model="latitude" type="number" placeholder="Latitude" />
+      <input v-model="longitude" type="number" placeholder="Longitude" />
+      <p>Already registered?</p>
       <button type="submit">Sign Up</button>
-      <br />
+      <hr />
       <button type="button" @click="changePage">Sign In</button>
     </form>
   </div>
