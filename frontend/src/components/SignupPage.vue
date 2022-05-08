90e72393fafb30c61d8e1064bd6cd365f99a5bf3
alan910127 <alan469357@gmail.com>
2022-05-08 17:07:40 +0800

    SignupPage

--- frontend/src/components/SignupPage.vue
+++ frontend/src/components/SignupPage.vue
@@ -1,9 +1,11 @@
 <template>
-<<<<<<< HEAD
-  <div class="sign"></div>
-=======
-  <h1>This is a SignupPage.</h1>
->>>>>>> d9b4107458d39be32478cad18277ee3490f7b3ce
+  <div class="sign">
+    <form @submit.prevent="onSubmit">
+      <input v-model="account" type="text" placeholder="Account" />
+      <input v-model="password" type="password" placeholder="Password" />
+      <button type="submit">Log In</button>
+    </form>
+  </div>
 </template>
 
 <script>
