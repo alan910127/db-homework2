b5423ccd6978b1e2eed18a7f0047a48071823971
alan910127 <alan469357@gmail.com>
2022-05-08 17:35:14 +0800

    SigninPage <-> SignupPage

--- frontend/src/components/SignupPage.vue
+++ frontend/src/components/SignupPage.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="sign">
+    <h3>Sign Up</h3>
     <form @submit.prevent="onSubmit">
       <input v-model="account" type="text" placeholder="Account" />
       <input v-model="password" type="password" placeholder="Password" />
-      <button type="submit">Log In</button>
+      <button type="submit">Sign Up</button>
+      <br />
+      <button type="button" @click="changePage">Sign In</button>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    changePage() {
+      this.$emit("change");
+    },
+  },
+};
 </script>
 
 <style>
