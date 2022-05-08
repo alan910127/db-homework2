6dc9f4be759cc7262cb4fb530f62f545b0658e60
alan910127 <alan469357@gmail.com>
2022-05-08 17:08:07 +0800

    LoginView: catch and emit 'success'

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="login">
     <h1>Login Page</h1>
-    <signin-page v-if="user === null"></signin-page>
+    <signin-page
+      v-if="flag === 'sign-in'"
+      @success="logInSuccessed"
+    ></signin-page>
     <signup-page v-else></signup-page>
   </div>
 </template>
@@ -13,12 +16,17 @@ import SignupPage from "@/components/SigninPage.vue";
 export default {
   data() {
     return {
-      user: null,
+      flag: "sign-in",
     };
   },
   components: {
     SigninPage,
     SignupPage,
+  },
+  methods: {
+    logInSuccessed(user) {
+      this.$emit("success", user);
+    },
   },
 };
 </script>
