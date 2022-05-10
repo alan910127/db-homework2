e55f6e78df557c40deaa6541905207cd9ffd4f89
alan910127 <alan469357@gmail.com>
2022-05-10 23:03:48 +0800

    Plugin: vuex-persistedstate

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,10 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import createPersistedState from "vuex-persistedstate";
 
 export default createStore({
+  plugins: [createPersistedState()],
   state: {
-    user: {},
+    user: null,
     shops: []
   },
   getters: {
