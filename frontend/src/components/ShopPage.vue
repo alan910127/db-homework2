7d4a726b4de90e45d1508823c755eb56d9fac7db
vinchantle <chenvincent610@gmail.com>
2022-05-12 00:00:24 +0800

    update ShopPage.vue

--- frontend/src/components/ShopPage.vue
+++ frontend/src/components/ShopPage.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <h1>Shop Page</h1>
+    <shop-register />
+    <add-meal />
   </div>
 </template>
 
 <script>
-export default {};
+import AddMeal from "./AddMeal.vue";
+import ShopRegister from "./ShopRegister.vue";
+export default {
+  components: { ShopRegister, AddMeal },
+};
 </script>
 
 <style>
