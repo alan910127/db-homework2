26ab4fc6610dc919a7a3b517a24b8ab702d8cece
alan910127 <alan469357@gmail.com>
2022-05-13 20:36:38 +0800

    Popup window

---
+++ frontend/src/components/PopupWindow.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="popup">
+    <div class="popup-inner">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-x-lg"
+        viewBox="0 0 16 16"
+        @click="this.$emit('closePopup')"
+        id="close"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"
+        />
+        <path
+          fill-rule="evenodd"
+          d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"
+        />
+      </svg>
+
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/global.scss";
+
+.popup {
+  @include flex;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 99;
+  background-color: rgba(0, 0, 0, 0.2);
+
+  .popup-inner {
+    background: var(--white-color);
+    padding: 32px;
+    margin: 20%;
+    border-radius: 8px;
+
+    #close {
+      cursor: pointer;
+    }
+  }
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,16 +4,27 @@
     <button type="button" @click="onClick">Logout</button>
     <nav-home />
     <router-view />
+    <popup-window v-if="showPopup" @closePopup="showPopup = false">
+      <h1>Popup!</h1>
+    </popup-window>
+    <button @click="showPopup = true">Show Popup</button>
   </div>
 </template>
 
 <script>
 import NavHome from "@/components/NavHome.vue";
+import PopupWindow from "../components/PopupWindow.vue";
 
 export default {
   name: "HomeView",
+  data() {
+    return {
+      showPopup: false,
+    };
+  },
   components: {
     NavHome,
+    PopupWindow,
   },
   methods: {
     onClick() {
