401c450755cf13d8dade07f8ae4ae85649d0190f
vinchantle <chenvincent610@gmail.com>
2022-05-15 13:22:58 +0800

    Add MealList component

--- frontend/src/components/ShopPage.vue
+++ frontend/src/components/ShopPage.vue
@@ -2,14 +2,17 @@
   <div>
     <shop-register />
     <add-meal />
+    <meal-list />
   </div>
 </template>
 
 <script>
 import AddMeal from "./AddMeal.vue";
 import ShopRegister from "./ShopRegister.vue";
+import MealList from "./MealList.vue";
+
 export default {
-  components: { ShopRegister, AddMeal },
+  components: { ShopRegister, AddMeal, MealList },
   methods: {
     changePage() {
       this.$router.push({ name: "homepage" });
