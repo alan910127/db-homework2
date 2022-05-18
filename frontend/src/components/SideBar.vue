ebe9704409448c3e94a5a8b0b92f33271d806a61
vinchantle <chenvincent610@gmail.com>
2022-05-18 22:19:43 +0800

    update style of location

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -141,31 +141,41 @@
             </svg>
             {{ user.phone }}
             <br />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              class="bi bi-geo-alt-fill"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"
-              />
-            </svg>
             <form @submit.prevent="onClick">
-              <input
-                type="number"
-                step="0.000000000000001"
-                v-model="form.latitude"
-                :disabled="!isEdit"
-              />
-              <input
-                type="number"
-                step="0.000000000000001"
-                v-model="form.longitude"
-                :disabled="!isEdit"
-              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                class="bi bi-geo-alt-fill"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"
+                />
+              </svg>
+              <div class="location">
+                <div class="lat">
+                  <label for="lat">lat </label>
+                  <input
+                    type="number"
+                    step="0.000000000000001"
+                    v-model="form.latitude"
+                    :disabled="!isEdit"
+                    id="lat"
+                  />
+                </div>
+                <div class="lng">
+                  <label for="long">lng </label>
+                  <input
+                    type="number"
+                    step="0.000000000000001"
+                    v-model="form.longitude"
+                    :disabled="!isEdit"
+                    id="long"
+                  />
+                </div>
+              </div>
               <span class="edit" @click="onClick()">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -376,6 +386,24 @@ aside {
       .text {
         color: var(--text-color);
         transition: 0.3s ease-out;
+      }
+    }
+    form {
+      @include flex-row;
+      align-items: center;
+
+      .location {
+        @include flex;
+        align-content: flex-start;
+
+        .lat .lng {
+          @include flex-row;
+        }
+      }
+
+      input {
+        border: none;
+        width: 50%;
       }
     }
 
