b78b77eef62e4e3e1330fb0b2c7399c5f4be8f4b
alan910127 <alan469357@gmail.com>
2022-05-20 00:51:19 +0800

    remove config.py

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-ALL_CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-BASE_DIR = os.path.abspath(os.getcwd())
-DB_NAME = 'db_homework.db'
-DEBUG=True
-
-DB_URI = f'sqlite:///{ BASE_DIR }/{ DB_NAME }'
